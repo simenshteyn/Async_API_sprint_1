17d53b6b5315d0e3cd149b9f3d274f0399879c6b
svvladimir-ru <svetlakov454@gmail.com>
2021-10-07 14:37:40 +0300

    Добавил в ETL загрузку person

---
+++ src/models/models.py
@@ -0,0 +1,46 @@
+from uuid import UUID
+from datetime import date
+
+# import orjson
+
+from typing import Union, Optional, List, Dict
+from pydantic import BaseModel
+
+
+OBJ_ID   = Union[str, str, UUID]
+OBJ_NAME = Union[str, str, UUID]
+
+
+# def orjson_dumps(v, *, default):
+#     return orjson.dumps(v, default=default).decode()
+
+
+class Film(BaseModel):
+    id              : Union[int, str, UUID]
+    imdb_rating     : Optional[float] = None
+    title           : str
+    description     : Optional[str] = None
+    actors_names    : Optional[List[str]] = None
+    writers_names   : Optional[List[str]] = None
+    directors_names : Optional[List[str]] = None
+    genres_names    : Optional[List[str]] = None
+    actors          : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
+    writers         : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
+    directors       : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
+
+    # class Config:
+    #     json_loads = orjson.loads
+    #     json_dumps = orjson_dumps
+
+
+class Genre(BaseModel):
+    id              : Union[int, str, UUID]
+    name            : str
+    description     : Optional[str] = None
+
+
+class Person(BaseModel):
+    id              : Union[int, str, UUID]
+    full_name       : str
+    birth_date      : date
+    roles           : List[str]  # под вопросом
--- src/services/film.py
+++ src/services/film.py
@@ -7,7 +7,7 @@ from fastapi import Depends
 
 from src.db.elastic import get_elastic
 from src.db.redis import get_redis
-from src.models.film import Film
+from src.models.models import Film
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
 
