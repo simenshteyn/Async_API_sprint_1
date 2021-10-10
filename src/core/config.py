42adfb4950fb93b8e23ecc6b5247bff77395b321
svvladimir-ru <svetlakov454@gmail.com>
2021-10-10 12:23:11 +0300

    Добавил init, правки в порте redis

--- src/core/config.py
+++ src/core/config.py
@@ -11,7 +11,7 @@ PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
 
 # Настройки Redis
 REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6380))
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
 # Настройки Elasticsearch
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
