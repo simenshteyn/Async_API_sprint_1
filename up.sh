7bb7167fe7e821ca41d3b4c34b45de2163cae7b7
svvladimir-ru <svetlakov454@gmail.com>
2021-10-10 09:35:15 +0300

    правки в up.sh

--- up.sh
+++ up.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+echo "Создание структуры папок для запуска ETL/ES. Запуск elastic"
+mkdir volumes
+mkdir volumes/elasticdb
+mkdir volumes/ETL
+touch volumes/ETL/etl.log
 cp .env.sample .env
+mkdir src/services/etl/src
+cp -r src/models src/services/etl/src/
+cp -r src/services/schemas_es src/services/etl/src/
+cp src/config.py src/services/etl/src/
+cp requirements.txt src/services/
 docker-compose up --build -d ma_postgres
 sleep 5
 echo "Начало загрузки данных в postgres"
@@ -9,19 +19,9 @@ cd sqlite_to_postgres/ || exit
 cd ..
 #echo "Запуск Nginx"
 #docker-compose up --build -d ma_nginx
-echo "Создание структуры папок для запуска ETL/ES. Запуск elastic"
-mkdir volumes
-mkdir volumes/elasticdb
-mkdir volumes/ETL
-touch volumes/ETL/etl.log
 docker-compose up --build -d ma_es01
 sleep 5
 echo "Копирование папок и файлов для сборки образа ETL"
-mkdir src/services/etl/src
-cp -r src/models src/services/etl/src/
-cp -r src/services/schemas_es src/services/etl/src/
-cp src/config.py src/services/etl/src/
-cp requirements.txt src/services/
 docker-compose up --build -d ma_etl
 sleep 20
 rm -r src/services/etl/src
