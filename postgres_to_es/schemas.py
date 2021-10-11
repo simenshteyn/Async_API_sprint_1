34c581ac2ed18bd99ae0e54b947f25cb2b3004cc
svvladimir-ru <svetlakov454@gmail.com>
2021-10-11 13:29:41 +0300

    правки в схеме

--- postgres_to_es/schemas.py
+++ postgres_to_es/schemas.py
@@ -1,7 +1,9 @@
+import orjson
+
 from uuid import UUID
 from datetime import date
 
-# import orjson
+import orjson
 
 from typing import Union, Optional, List, Dict
 from pydantic import BaseModel
@@ -11,32 +13,25 @@ OBJ_ID   = Union[str, str, UUID]
 OBJ_NAME = Union[str, str, UUID]
 
 
-# def orjson_dumps(v, *, default):
-#     return orjson.dumps(v, default=default).decode()
-
-
-class Roles(BaseModel):
-    actor: Optional[str] = None
-    writers: Optional[str] = None
-    director: Optional[str] = None
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
 
 
 class Film(BaseModel):
     id              : Union[int, str, UUID]
     imdb_rating     : Optional[float] = None
+    genre           : Optional[List[str]] = None
     title           : str
     description     : Optional[str] = None
+    director        : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
     actors_names    : Optional[List[str]] = None
     writers_names   : Optional[List[str]] = None
-    directors_names : Optional[List[str]] = None
-    genres_names    : Optional[List[str]] = None
     actors          : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
     writers         : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
-    directors       : Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
 
-    # class Config:
-    #     json_loads = orjson.loads
-    #     json_dumps = orjson_dumps
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
 
 
 class Genre(BaseModel):
@@ -44,13 +39,17 @@ class Genre(BaseModel):
     name            : str
     description     : Optional[str] = None
 
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
 
 class Person(BaseModel):
     id              : Union[int, str, UUID]
     full_name       : str
     birth_date      : Optional[date] = None
-    roles           : list[dict]  # под вопросом
+    roles           : List[str]
 
-    # actor = dict(row).get('actor'),
-    # writer = dict(row).get('writer'),
-    # director = dict(row).get('director'),
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
