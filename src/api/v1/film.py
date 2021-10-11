d4bf7599ff4bdad18587476d5a012e1a9fc37bb2
Sergey Simenshteyn <simenshteyn@gmail.com>
2021-10-11 19:02:31 +0700

    Search film by UUID in FastAPI on URL api/v1/film/uuid

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -1,34 +1,46 @@
 from http import HTTPStatus
+from typing import Union, Optional, List, Dict
+from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 
-
-from services.film import FilmService, get_film_service
+from src.services.film import FilmService, get_film_service
 
 
 router = APIRouter()
 
 
+OBJ_ID = Union[str, str, UUID]
+OBJ_NAME = Union[str, str, UUID]
+
 class Film(BaseModel):
-    id: str
+    id: Union[int, str, UUID]
+    imdb_rating: Optional[float] = None
+    genre: Optional[List[str]] = None
     title: str
+    description: Optional[str] = None
+    director: Optional[List[str]] = None
+    actors_names: Optional[List[str]] = None
+    writers_names: Optional[List[str]] = None
+    actors: Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
+    writers: Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
 @router.get('/{film_id}', response_model=Film)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+async def film_details(film_id: str,
+                       film_service: FilmService = Depends(get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
-                # Такой код будет более поддерживаемым
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
-
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description
-        # Которое отсутствует в модели ответа API.
-        # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
-        # вы бы предоставляли клиентам данные, которые им не нужны
-        # и, возможно, данные, которые опасно возвращать
-    return Film(id=film.id, title=film.title)
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='film not found')
+    return Film(id=film.id,
+                imdb_rating=film.imdb_rating,
+                genre=film.genre,
+                title=film.title,
+                description=film.description,
+                director=film.director,
+                actors_names=film.actors_names,
+                writers_names=film.writers_names,
+                actors=film.actors,
+                writers=film.writers)
