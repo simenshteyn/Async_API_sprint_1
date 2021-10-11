e7a513d9f210ca61ecf6c83fcc4b6c3a85120c8f
Sergey Simenshteyn <simenshteyn@gmail.com>
2021-10-12 00:22:59 +0700

    Add search my movie title with cached elastic

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -26,6 +26,31 @@ class Film(BaseModel):
     actors: Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
     writers: Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
 
+class FilmShort(BaseModel):
+    id: Union[int, str, UUID]
+    title: str
+    imdb_rating: Optional[float] = None
+
+
+@router.get('/', response_model=List[Film])
+async def films_sorted():
+    return [Film(id='12345', title='Test_film', imdb_rating=7.5),
+            Film(id='12345', title='Second_film', imdb_rating=6.5)]
+
+@router.get('/search/{film_search_string}', response_model=List[FilmShort])
+async def films_search(film_search_string: str,
+                       film_service: FilmService = Depends(get_film_service)) -> List[FilmShort]:
+    film_list = await film_service.get_by_search(film_search_string)
+    if not film_list:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='film not found')
+    result = []
+    for film in film_list:
+        result.append(FilmShort(id=film.id,
+                                title=film.title,
+                                imdb_rating=film.imdb_rating,))
+    return result
+
 
 @router.get('/{film_id}', response_model=Film)
 async def film_details(film_id: str,
--- src/services/film.py
+++ src/services/film.py
@@ -1,14 +1,16 @@
+import json
 from functools import lru_cache
-from typing import Optional
+from typing import Optional, List
 
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
+from pydantic import parse_raw_as
+from pydantic.json import pydantic_encoder
 
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.models import Film
-
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
@@ -27,21 +29,61 @@ class FilmService:
             await self._put_film_to_cache(film)
         return film
 
+    async def get_by_search(self, search_string: str) -> Optional[List[Film]]:
+        film_list = await self._film_search_from_cache(search_string)
+        if not film_list:
+            film_list = await self._search_film_from_elastic(search_string)
+            if not film_list:
+                return None
+            await self._put_film_search_to_cache(search_string, film_list)
+        return film_list
+
+
     async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
         doc = await self.elastic.get('movies', film_id)
         return Film(**doc['_source'])
+
+    async def _search_film_from_elastic(self, search_string: str) -> Optional[
+        List[Film]]:
+        doc = await self.elastic.search(
+            index='movies',
+            body={"query": {
+                "match": {
+                    "title": {
+                        "query": search_string,
+                        "fuzziness": "auto"
+                    }
+                }
+            }})
+        result = []
+        for movie in doc['hits']['hits']:
+            result.append(Film(**movie['_source']))
+        return result
 
     async def _film_from_cache(self, film_id: str) -> Optional[Film]:
         data = await self.redis.get(film_id)
         if not data:
             return None
-
         film = Film.parse_raw(data)
         return film
+
+    async def _film_search_from_cache(self, search_string: str) -> Optional[
+        List[Film]]:
+        data = await self.redis.get(search_string)
+        if not data:
+            return None
+        film_list = parse_raw_as(List[Film], data)
+        return film_list
 
     async def _put_film_to_cache(self, film: Film):
         await self.redis.set(film.id, film.json(),
                              expire=FILM_CACHE_EXPIRE_IN_SECONDS)
+
+    async def _put_film_search_to_cache(self,
+                                        search_string: str,
+                                        film_list: List[Film]):
+        film_list_json = json.dumps(film_list, default=pydantic_encoder)
+        await self.redis.set(search_string, film_list_json)
 
 
 @lru_cache()
