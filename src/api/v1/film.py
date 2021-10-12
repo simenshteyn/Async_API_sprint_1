01f85d24cdd631065fcc12b2e2cbff503998c39f
Sergey Simenshteyn <simenshteyn@gmail.com>
2021-10-12 14:47:22 +0700

    Exclude unset fields from api response in Film API
    Took 17 minutes

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -7,12 +7,11 @@ from pydantic import BaseModel
 
 from services.film import FilmService, get_film_service
 
-
 router = APIRouter()
-
 
 OBJ_ID = Union[str, str, UUID]
 OBJ_NAME = Union[str, str, UUID]
+
 
 class Film(BaseModel):
     id: Union[int, str, UUID]
@@ -26,6 +25,7 @@ class Film(BaseModel):
     actors: Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
     writers: Optional[List[Dict[OBJ_ID, OBJ_NAME]]] = None
 
+
 class FilmShort(BaseModel):
     id: Union[int, str, UUID]
     title: str
@@ -37,9 +37,11 @@ async def films_sorted():
     return [Film(id='12345', title='Test_film', imdb_rating=7.5),
             Film(id='12345', title='Second_film', imdb_rating=6.5)]
 
+
 @router.get('/search/{film_search_string}', response_model=List[FilmShort])
 async def films_search(film_search_string: str,
-                       film_service: FilmService = Depends(get_film_service)) -> List[FilmShort]:
+                       film_service: FilmService = Depends(
+                           get_film_service)) -> List[FilmShort]:
     film_list = await film_service.get_by_search(film_search_string)
     if not film_list:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
@@ -48,13 +50,15 @@ async def films_search(film_search_string: str,
     for film in film_list:
         result.append(FilmShort(id=film.id,
                                 title=film.title,
-                                imdb_rating=film.imdb_rating,))
+                                imdb_rating=film.imdb_rating, ))
     return result
 
 
-@router.get('/{film_id}', response_model=Film)
+@router.get('/{film_id}', response_model=Film,
+            response_model_exclude_unset=True)
 async def film_details(film_id: str,
-                       film_service: FilmService = Depends(get_film_service)) -> Film:
+                       film_service: FilmService = Depends(
+                           get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
