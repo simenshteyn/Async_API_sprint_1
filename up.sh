9a0864ec6347e2b438f9f335f516ce6314201ad1
svvladimir-ru <svetlakov454@gmail.com>
2021-10-14 11:56:26 +0300

    Избавился от переноса данных из sqlite

--- up.sh
+++ up.sh
@@ -7,13 +7,8 @@ mkdir volumes/postgres_data
 touch volumes/ETL/etl.log
 cp requirements.txt src/
 cp .env.sample .env
+echo "Запуск postgres"
 docker-compose up --build -d ma_postgres
-sleep 25
-echo "Начало загрузки данных в postgres"
-cp .env.sample sqlite_to_postgres/.env
-cd sqlite_to_postgres/ || exit
-../venv/bin/python load_data.py
-cd ..
 #echo "Запуск Nginx"
 #docker-compose up --build -d ma_nginx
 echo 'Start Redis'
